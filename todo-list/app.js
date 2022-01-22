dc8c7beadbcc1670752b982a9fb5d9dc8ac2acde
Vitaly Undro <vitalyundro@gmail.com>
2022-01-22 19:40:34 +0300

    feat: add the ability to add tasks

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,34 +1,132 @@
-const tasks = [
-  {
+const tasks = [{
     _id: '5d2ca9e2e03d40b326596aa7',
     completed: true,
-    body:
-      'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+    body: 'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
     title: 'Eu ea incididunt sunt consectetur fugiat non.',
   },
   {
     _id: '5d2ca9e29c8a94095c1288e0',
     completed: false,
-    body:
-      'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
-    title:
-      'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+    body: 'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+    title: 'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
   },
   {
     _id: '5d2ca9e2e03d40b3232496aa7',
     completed: true,
-    body:
-      'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+    body: 'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
     title: 'Eu ea incididunt sunt consectetur fugiat non.',
   },
   {
     _id: '5d2ca9e29c8a94095564788e0',
     completed: false,
-    body:
-      'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
-    title:
-      'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+    body: 'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+    title: 'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
   },
 ];
 
-(function(arrOfTasks) {})(tasks);
+(function (arrOfTasks) {
+
+  const objOfTask = arrOfTasks.reduce((acc, task) => {
+    acc[task._id] = task;
+    return acc;
+  }, {});
+
+  //Elements UI
+
+  const listContainer = document.querySelector('.tasks-list-section .list-group'),
+    form = document.forms['addTask'],
+    inputTitle = form.elements['title'],
+    inputBody = form.elements['body'];
+
+
+  //Events
+  renaderAllTasks(objOfTask);
+  form.addEventListener('submit', onFormSubmitHandler);
+
+  function renaderAllTasks(taskList) {
+    if (!taskList) {
+      console.error('Передайте список задач');
+      return;
+    }
+
+    const fragment = document.createDocumentFragment();
+
+    Object.values(taskList).forEach(task => {
+      const li = listItemTemplate(task);
+      fragment.appendChild(li);
+    });
+
+    listContainer.appendChild(fragment);
+
+
+  }
+
+
+  function listItemTemplate({
+    _id,
+    title,
+    body
+  } = {}) {
+
+    const li = document.createElement('li'),
+      span = document.createElement('span'),
+      deleteBtn = document.createElement('button'),
+      article = document.createElement('p');
+
+    li.classList.add('list-group-item', 'd-flex', 'align-items-center', 'flex-wrap', 'mt-2');
+
+    span.textContent = title;
+    span.style.fontWeight = 'bold';
+
+    deleteBtn.textContent = 'Delete task';
+    deleteBtn.classList.add('btn', 'btn-danger', 'ml-auto', 'delete-btn');
+
+    article.textContent = body;
+    article.classList.add('mt-2', 'w-100');
+
+    li.appendChild(span);
+    li.appendChild(deleteBtn);
+    li.appendChild(article);
+
+    return li;
+  }
+
+  function onFormSubmitHandler(e) {
+    e.preventDefault();
+    const titleValue = inputTitle.value,
+      bodyValue = inputBody.value;
+
+
+
+    if (!titleValue || !bodyValue) {
+      alert('Please enter "Task title" and "Task body"');
+      return;
+    }
+
+    const task = createNewTask(titleValue, bodyValue),
+      listItem = listItemTemplate(task);
+
+    listContainer.insertAdjacentElement('afterbegin', listItem);
+    form.reset();
+  }
+
+
+  function createNewTask(title, body) {
+    const newTask = {
+      title,
+      body,
+      completed: false,
+      _id: `task-${Math.random()}`
+    }
+
+    objOfTask[newTask._id] = newTask;
+
+
+
+    return {
+      ...newTask
+    };
+
+  }
+
+})(tasks);
